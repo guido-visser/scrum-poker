088db841b170438523b372b0c1720c34f0727c38
Guido Visser <guido.vsr@gmail.com>
2022-03-12 23:42:08 +0100

    Removed craco-less plugin

--- craco.config.js
+++ craco.config.js
@@ -1,20 +1,6 @@
-const CracoLessPlugin = require("craco-less");
 const path = require("path");
 
 module.exports = {
-    plugins: [
-        {
-            plugin: CracoLessPlugin,
-            options: {
-                lessLoaderOptions: {
-                    lessOptions: {
-                        modifyVars: { "@primary-color": "#f00" },
-                        javascriptEnabled: true,
-                    },
-                },
-            },
-        },
-    ],
     webpack: {
         alias: {
             "@": path.join(path.resolve(__dirname, "./src")),
